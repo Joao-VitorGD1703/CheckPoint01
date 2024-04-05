45f071987932003f751993bd2ae2e726a6331374
Joao-VitorGD1703 <douvernyjoaovitor@gmail.com>
2024-04-04 22:45:41 -0300

    Adicionei o ts

---
+++ tipos.ts
@@ -0,0 +1,11 @@
+// tipos.ts
+
+// Declaração das variáveis
+let numero: number;
+let texto: string;
+let ativo: boolean;
+let arrayNumeros: number[];
+let pessoa: pessoa;
+
+type pessoa = { nome: string, idade: number };
+
